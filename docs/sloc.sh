7461e01b6537ebb90d0bfe40e8d354eeaac5c915
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2026-07-06 12:19:13 +0300

    improve sloc

--- docs/sloc.sh
+++ docs/sloc.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "Counting lines of code in Go files..."
 find . -name "*.go" | xargs wc -l
+echo -e "\nCounting lines of code in Markdown files..."
+find . -name "*.md" | xargs wc -l
