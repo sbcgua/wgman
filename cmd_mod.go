bf78aad73fccf3c895f5b95b63efd4218974bfa1
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2026-07-09 12:15:56 +0300

    more utils

--- cmd_mod.go
+++ cmd_mod.go
@@ -405,15 +405,3 @@ func normalizeAccessSet(set map[string]bool) []string {
 	sort.Strings(entries)
 	return entries
 }
-
-func sameStringSlices(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
--- utils.go
+++ utils.go
@@ -60,3 +60,15 @@ func caseFold(s string) string {
 	}
 	return string(b)
 }
+
+func sameStringSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
